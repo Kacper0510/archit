de59118d119e2015f69ebcd9c8e4e19139eea062
Kacper0510 <wkacper@spoko.pl>
2025-03-29 21:46:00 +0100

    Add Gradle application plugin

---
+++ src/main/java/archit/common/Utils.java
@@ -0,0 +1,9 @@
+package archit.common;
+
+public class Utils {
+    private Utils() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static final String TEST_MESSAGE = "Hello, Archit!";  // TODO remove
+}
