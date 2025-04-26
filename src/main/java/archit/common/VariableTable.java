ba4d2c28e1e56bdc8a93c21227cfee6f8318c8a0
Atras19 <emil.wajda.ew@gmail.com>
2025-04-26 20:56:42 +0200

    add VariableException

--- src/main/java/archit/common/VariableTable.java
+++ src/main/java/archit/common/VariableTable.java
@@ -4,14 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class VariableTable {
-    private final Map<String, String> variables = new HashMap<String, String>();
+    private final Map<String, String> variables = new HashMap<>();
 
     public void declareVariable(String name, String type){
         if (!variables.containsKey(name)) {
             variables.put(name, type);
         }
         else{
-            throw new RuntimeException("Variable '" + name + "' already exists");
+            throw new VariableException("Variable '" + name + "' already exists");
         }
     }
 
@@ -20,7 +20,17 @@ public class VariableTable {
             return variables.get(name);
         }
         else{
-            throw new RuntimeException("Variable '" + name + "' does not exist");
+            throw new VariableException("Variable '" + name + "' does not exist");
+        }
+    }
+
+    public Boolean isDeclared(String name) {
+        return variables.containsKey(name);
+    }
+
+    public static class VariableException extends RuntimeException {
+        public VariableException(String message) {
+            super(message);
         }
     }
 
