30579a8b15b05a5b2faeb25bae109c25f8ae8773
Kacper0510 <wkacper@spoko.pl>
2025-04-27 15:58:35 +0200

    Add Type class

---
+++ src/main/java/archit/common/Type.java
@@ -0,0 +1,161 @@
+package archit.common;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class Type {
+    private final String name;
+    private final Class<?> equivalent;
+
+    private Type(String name, Class<?> equivalent) {
+        this.name = name;
+        this.equivalent = equivalent;
+    }
+
+    public final String getBaseName() {
+        return name;
+    }
+
+    public final Class<?> getEquivalent() {
+        return equivalent;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public final int hashCode() {
+        return toString().hashCode();
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (o instanceof Type) {
+            return toString().equals(o.toString());
+        }
+        return false;
+    }
+
+    public static class ListType extends Type {
+        private final Type elements;
+
+        private ListType(Type elements) {
+            super("list", ArrayList.class);
+            this.elements = elements;
+        }
+
+        public final Type getElements() {
+            return elements;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + elements + "]";
+        }
+
+        @Override
+        public ListType asListType() {
+            return this;
+        }
+    }
+
+    public static class MapType extends Type {
+        private final Type key;
+        private final Type value;
+
+        private MapType(Type key, Type value) {
+            super("map", HashMap.class);
+            this.key = key;
+            this.value = value;
+        }
+
+        public final Type getKey() {
+            return key;
+        }
+
+        public final Type getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            var sb = new StringBuilder();
+            sb.append('|');
+            sb.append(key);
+            sb.append(" -> ");
+            sb.append(value);
+            sb.append('|');
+            return sb.toString();
+        }
+
+        @Override
+        public MapType asMapType() {
+            return this;
+        }
+    }
+
+    public static class LiteralType extends Type {
+        private final SortedSet<String> members;
+
+        public LiteralType(String[] members) {
+            super("literal", String.class);
+            this.members = Collections.unmodifiableSortedSet(new TreeSet<>(Arrays.asList(members)));
+        }
+
+        public final SortedSet<String> getMembers() {
+            return members;
+        }
+
+        public final boolean contains(String member) {
+            return members.contains(member);
+        }
+
+        @Override
+        public String toString() {
+            return "<" + String.join(", ", members) + ">";
+        }
+
+        @Override
+        public LiteralType asLiteralType() {
+            return this;
+        }
+    }
+
+    public static final Type number = new Type("number", BigInteger.class);
+    public static final Type real = new Type("real", Double.class);
+    public static final Type logic = new Type("logic", Boolean.class);
+    public static final Type string = new Type("string", String.class);
+    public static final Type material = new Type("material", Material.class);
+
+    public static Type list(Type elements) {
+        return new ListType(elements);
+    }
+
+    public ListType asListType() {
+        return null;
+    }
+
+    public static Type map(Type key, Type value) {
+        return new MapType(key, value);
+    }
+
+    public MapType asMapType() {
+        return null;
+    }
+
+    // chciałem nazwać 'enum' jak w prezentacji, ale jest to słowo kluczowe...
+    public static Type literal(String... values) {
+        return new LiteralType(values);
+    }
+
+    public LiteralType asLiteralType() {
+        return null;
+    }
+}
