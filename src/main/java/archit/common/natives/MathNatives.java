9237c13a6209cc42f4323d8f997dd4d76c5e4265
Atras19 <emil.wajda.ew@gmail.com>
2025-06-21 17:55:40 +0200

    add some Math to stdl

--- src/main/java/archit/common/natives/MathNatives.java
+++ src/main/java/archit/common/natives/MathNatives.java
@@ -39,4 +39,31 @@ public class MathNatives {
     public BigInteger round(ScriptRun run, Double value) {
         return BigInteger.valueOf(Math.round(value));
     }
+
+    @ArchitNative("native sin(value: real): real;")
+    public Double sin(ScriptRun run, Double value) {return Math.sin(value);}
+
+    @ArchitNative("native cos(value: real): real;")
+    public Double cos(ScriptRun run, Double value) {return Math.cos(value);}
+
+    @ArchitNative("native tan(value: real): real;")
+    public Double tan(ScriptRun run, Double value) {return Math.tan(value);}
+
+    @ArchitNative("native log(base: real, value: real): real;")
+    public Double log(ScriptRun run, Double base, Double value) {return Math.log(value) / Math.log(base);}
+
+    @ArchitNative("native log(base: number, value: number): real;")
+    public Double log(ScriptRun run, BigInteger base, BigInteger value) {return Math.log(value.doubleValue()) / Math.log(base.doubleValue());}
+
+    @ArchitNative("native sign(value: real): number;")
+    public BigInteger sign(ScriptRun run, Double value) {return BigInteger.valueOf(Long.signum((long) Math.signum(value)));}
+
+    @ArchitNative("native sign(value: number): number;")
+    public BigInteger sign(ScriptRun run, BigInteger value) {return BigInteger.valueOf(value.signum());}
+
+    @ArchitNative("native toRadians(deg: real): real;")
+    public Double toRadians(ScriptRun run, Double deg) {return Math.toRadians(deg);}
+
+    @ArchitNative("native toDegrees(rad: real): real;")
+    public Double toDegrees(ScriptRun run, Double rad) {return Math.toDegrees(rad);}
 }
